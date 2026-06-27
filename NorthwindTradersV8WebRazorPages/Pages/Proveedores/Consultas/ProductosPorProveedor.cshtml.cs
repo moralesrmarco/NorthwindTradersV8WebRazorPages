9cfa90608da3f806009840c413256d2eab75abe0
moralesrmarco <marco2003_6@hotmail.com>
2026-06-27 16:36:54 -0600

    Se modificó _Layout

---
+++ NorthwindTradersV8WebRazorPages/Pages/Proveedores/Consultas/ProductosPorProveedor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NorthwindTradersV8WebRazorPages.Pages.Proveedores.Consultas
+{
+    public class ProductosPorProveedorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
