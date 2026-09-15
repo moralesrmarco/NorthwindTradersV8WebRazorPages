c0ec032f6b1a2b1426c4a704e44739ffc69c1080
moralesrmarco <marco2003_6@hotmail.com>
2026-09-15 02:06:19 -0600

    Se modificó el inicio de la aplicación

--- NorthwindTradersV8WebRazorPages/Pages/Index.cshtml.cs
+++ NorthwindTradersV8WebRazorPages/Pages/Index.cshtml.cs
@@ -1,13 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NorthwindTradersV8WebRazorPages.Common;
 
 namespace NorthwindTradersV8WebRazorPages.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            if (User.TienePermiso(Permisos.TableroAltaDireccion))
+            {
+                return RedirectToPage(
+                    "/Tableros/AltaDireccion/TableroControlAltaDireccion");
+            }
 
+            if (User.TienePermiso(Permisos.TableroVendedores))
+            {
+                return RedirectToPage(
+                    "/Tableros/Vendedores/TableroControlVendedores");
+            }
+
+            return Page();
         }
     }
 }
